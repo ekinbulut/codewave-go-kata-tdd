e0f70be455d9ecb6b0a517058c34c6eadf618658
ekinbulut <ekin.bulut@deliveryhero.com>
2022-06-17 16:29:15 +0300

    StringCalculator created with Add signature

---
+++ internals/stringcalculator.go
@@ -0,0 +1,12 @@
+package internals
+
+type StringCalculator struct {
+}
+
+func NewStringCalculator() *StringCalculator {
+	return &StringCalculator{}
+}
+
+func (sc *StringCalculator) Add(s string) (result int) {
+	return 0
+}
