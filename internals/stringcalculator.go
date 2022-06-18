c979b10add75f7729cd97998aea7ca75ce0e0b95
ekinbulut <ekinbulut@gmail.com>
2022-06-18 19:32:59 +0300

    Green test: Support different delimiters

--- internals/stringcalculator.go
+++ internals/stringcalculator.go
@@ -1,6 +1,7 @@
 package internals
 
 import (
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -13,14 +14,30 @@ func NewStringCalculator() *StringCalculator {
 }
 
 func (sc *StringCalculator) Add(s string) (result int) {
-	if len(s) == 0 {
+	if IsStringEmpty(s) {
 		return 0
 	}
 
-	ns := strings.Replace(s, "\n", ",", -1)
-	nums := strings.Split(ns, ",")
+	// create a regexp to match the delimiter and the numbers
+	re := regexp.MustCompile("^//(.)\n(.*)$")
+	match := re.FindStringSubmatch(s)
+	if len(match) > 0 {
+		delimiter := match[1]
+		numbers := match[2]
+		return ReplaceAndSplit(numbers, delimiter)
+	} else {
+		return ReplaceAndSplit(s, "\n")
+	}
+}
 
+func IsStringEmpty(s string) bool {
+	return len(s) == 0
+}
+
+func ReplaceAndSplit(s string, d string) (result int) {
 	var sum int
+	ns := strings.Replace(s, d, ",", -1)
+	nums := strings.Split(ns, ",")
 
 	for i := 0; i < len(nums); i++ {
 		n, err := strconv.Atoi(nums[i])
