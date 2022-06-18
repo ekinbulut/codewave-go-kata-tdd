02b7cb6763c0f8378735bdc6049589526431ba3e
ekinbulut <ekin.bulut@deliveryhero.com>
2022-06-18 10:51:39 +0300

    Red test: Support different delimiters

--- tests/string_calculator_test.go
+++ tests/string_calculator_test.go
@@ -19,4 +19,5 @@ func Test_Add_With_String(t *testing.T) {
 	assert.Equal(t, 1, c.Add("1"))
 	assert.Equal(t, 3, c.Add("1,2"))
 	assert.Equal(t, 6, c.Add("1\n2,3"))
+	assert.Equal(t, 6, c.Add("//;\n1;2"))
 }
