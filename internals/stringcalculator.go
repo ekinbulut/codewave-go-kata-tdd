52ca852cb187fc279163301af3550cccc3d99ec7
ekinbulut <ekin.bulut@deliveryhero.com>
2022-06-18 10:49:16 +0300

    Allow the Add method to handle new lines between numbers (instead of commas)

--- internals/stringcalculator.go
+++ internals/stringcalculator.go
@@ -1,6 +1,9 @@
 package internals
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 type StringCalculator struct {
 }
@@ -13,6 +16,19 @@ func (sc *StringCalculator) Add(s string) (result int) {
 	if len(s) == 0 {
 		return 0
 	}
-	nums := strings.Split(s, ",")
-	return 1
+
+	ns := strings.Replace(s, "\n", ",", -1)
+	nums := strings.Split(ns, ",")
+
+	var sum int
+
+	for i := 0; i < len(nums); i++ {
+		n, err := strconv.Atoi(nums[i])
+		if err != nil {
+			return 0
+		}
+		sum += n
+	}
+
+	return sum
 }
--- tests/string_calculator_test.go
+++ tests/string_calculator_test.go
@@ -11,6 +11,12 @@ func Test_Add_With_Empty_String(t *testing.T) {
 
 	c := sc.NewStringCalculator()
 	assert.Equal(t, 0, c.Add(""))
+}
+
+func Test_Add_With_String(t *testing.T) {
+
+	c := sc.NewStringCalculator()
 	assert.Equal(t, 1, c.Add("1"))
-	assert.Equal(t, 1, c.Add("1,2"))
+	assert.Equal(t, 3, c.Add("1,2"))
+	assert.Equal(t, 6, c.Add("1\n2,3"))
 }
